d2522cda9a137cd3e692b92a11b22e3fb1cfd0f4
Andres Court <andres@andrescourt.com>
2023-11-28 17:59:23 -0500

    Initial Commit

---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bca-go-final/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic("cannot start server")
+	}
+}
