94b548ce8932c7b38425e8010f137bcfd835b403
Andres Court <andres@andrescourt.com>
2023-11-28 20:06:15 -0500

    Added CORS Middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,8 +13,23 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	r.HandleFunc("/", s.HelloWorldHandler)
 	r.HandleFunc("/health", s.healthHandler)
+	r.Use(middleware)
 
 	return r
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request: %s %s\n", r.Method, r.URL)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		log.Printf("CORS Headers set")
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
