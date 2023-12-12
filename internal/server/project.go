65137335e844122048b320aa4e3fe4de258cecd7
Andres Court <andres@andrescourt.com>
2023-12-11 20:59:37 -0500

    Get all projects

--- internal/server/project.go
+++ internal/server/project.go
@@ -1,13 +1,26 @@
 package server
 
 import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (s *Server) AllProjects(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodOptions:
 		w.WriteHeader(http.StatusOK)
 	case http.MethodGet:
+		projects, err := s.DB.GetAllProjects(ctxPayload.CompanyId)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			resp["error"] = err.Error()
+			json.NewEncoder(w).Encode(resp)
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(projects)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
