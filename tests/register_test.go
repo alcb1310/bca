9d54815f23ab46e562ce1d9a3fff7cfc7ec8f4c3
Andres Court <andres@andrescourt.com>
2023-12-19 18:00:39 -0500

    Register tests use assert library

--- tests/register_test.go
+++ tests/register_test.go
@@ -5,13 +5,17 @@ import (
 	"bca-go-final/internal/types"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRegistrationRouteValidation(t *testing.T) {
+	assert := assert.New(t)
 	s := &server.Server{}
 	server := httptest.NewServer(http.HandlerFunc(s.Register))
 	defer server.Close()
@@ -23,67 +27,51 @@ func TestRegistrationRouteValidation(t *testing.T) {
 	defer resp.Body.Close()
 
 	// Assertions
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected := "{\"error\":\"EOF\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c := &types.CompanyCreate{}
 
 	buf := new(bytes.Buffer)
 	err = json.NewEncoder(buf).Encode(c)
 	resp, err = http.Post(server.URL, "application/json", buf)
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	defer resp.Body.Close()
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"name cannot be empty\",\"field\":\"name\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Name = "test"
 	err = json.NewEncoder(buf).Encode(c)
 	resp, err = http.Post(server.URL, "application/json", buf)
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	defer resp.Body.Close()
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"ruc cannot be empty\",\"field\":\"ruc\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Ruc = "123"
 	err = json.NewEncoder(buf).Encode(c)
 	resp, err = http.Post(server.URL, "application/json", buf)
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	defer resp.Body.Close()
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"should pass at least one employee\",\"field\":\"employees\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	type BadRequest struct {
 		Name      string `json:"name"`
@@ -101,18 +89,14 @@ func TestRegistrationRouteValidation(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error making request to server. Err: %v", err)
 	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"employees must be a number\",\"field\":\"employees\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Employees = 1
 	if err := json.NewEncoder(buf).Encode(&c); err != nil {
@@ -123,34 +107,26 @@ func TestRegistrationRouteValidation(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error making request to server. Err: %v", err.Error())
 	}
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"email cannot be empty\",\"field\":\"email\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Email = "test"
 	if err := json.NewEncoder(buf).Encode(&c); err != nil {
 		t.Fatalf("error marshaling. Err: %v", err)
 	}
 	resp, err = http.Post(server.URL, "application/json", buf)
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"invalid email\",\"field\":\"email\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Email = "LJGQ6@example.com"
 	if err := json.NewEncoder(buf).Encode(&c); err != nil {
@@ -160,17 +136,13 @@ func TestRegistrationRouteValidation(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error making request to server. Err: %v", err.Error())
 	}
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"password cannot be empty\",\"field\":\"password\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 
 	c.Password = "test"
 	if err := json.NewEncoder(buf).Encode(&c); err != nil {
@@ -180,15 +152,11 @@ func TestRegistrationRouteValidation(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error making request to server. Err: %v", err.Error())
 	}
-	if resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status bad request; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("expected status bad request; got %v", resp.Status))
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
 	expected = "{\"error\":\"name of the user cannot be empty\",\"field\":\"user\"}"
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 }
