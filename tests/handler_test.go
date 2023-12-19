8ca4e4a1c8862991130d3e7e480ec9f56df67ee0
Andres Court <andres@andrescourt.com>
2023-12-19 18:15:43 -0500

    Default handler test with assert

--- tests/handler_test.go
+++ tests/handler_test.go
@@ -2,13 +2,17 @@ package tests
 
 import (
 	"bca-go-final/internal/server"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHandler(t *testing.T) {
+	assert := assert.New(t)
 	s := &server.Server{}
 	server := httptest.NewServer(http.HandlerFunc(s.HelloWorldHandler))
 	defer server.Close()
@@ -18,15 +22,11 @@ func TestHandler(t *testing.T) {
 	}
 	defer resp.Body.Close()
 	// Assertions
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("expected status OK; got %v", resp.Status)
-	}
+	assert.Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("expected status OK; got %v", resp.Status))
 	expected := "{\"message\":\"Hello World\"}"
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
-	}
+	assert.Equal(expected, string(body), fmt.Sprintf("expected response body to be %v; got %v", expected, string(body)))
 }
