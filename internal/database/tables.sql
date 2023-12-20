2b0d06e7c543ff5619be819640d26f66e9bf0dec
Andres Court <andres@andrescourt.com>
2023-12-19 19:15:11 -0500

    Budget Items database schema model

--- internal/database/tables.sql
+++ internal/database/tables.sql
@@ -63,3 +63,19 @@ create table if not exists supplier (
      unique (supplier_id, company_id),
      unique (name, company_id)
 );
+
+create table if not exists budget_item(
+     id uuid PRIMARY KEY default gen_random_uuid(),
+     code text not null,
+     name text not null,
+     level smallint not null default 1,
+     accumulate boolean not null default true,
+
+     parent_id uuid references budget_item(id) on delete restrict,
+
+     company_id uuid not null references company(id) on delete restrict,
+     created_at timestamp with time zone default now(),
+
+     unique (code, company_id),
+     unique (name, company_id)
+);
