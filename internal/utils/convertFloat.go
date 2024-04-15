6959684ac37c71fbd84ed1aebb2fde376359837f
Andres Court <andres@andrescourt.com>
2024-04-15 13:27:37 -0500

    Convert string to float w/ testing

---
+++ internal/utils/convertFloat.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func ConvertFloat(value, field string, required bool) (val float64, err error) {
+	if required && value == "" {
+		return 0, errors.New(fmt.Sprintf("%s es requerido", field))
+	} else if value == "" {
+		return 0, nil
+	}
+
+	val, err = strconv.ParseFloat(value, 64)
+
+	if err != nil {
+		err = errors.New(fmt.Sprintf("%s debe ser un número válido", field))
+	}
+
+	if val < 0 {
+		err = errors.New(fmt.Sprintf("%s debe ser un número positivo", field))
+	}
+
+	return
+}
