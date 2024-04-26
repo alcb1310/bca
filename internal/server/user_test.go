0cee2cbf0c501984040986654958e94c5a411891
Andres Court <andres@andrescourt.com>
2024-04-26 18:46:13 -0500

    Create the user test file skeleton

---
+++ internal/server/user_test.go
@@ -0,0 +1,38 @@
+package server
+
+import "testing"
+
+// TEST: Profile function
+func TestProfile(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: Admin function
+func TestAdmin(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: ChangePassword function
+func TestChangePassword(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: SingleUser function
+func TestSingleUser(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: UsersTable function
+func TestUsersTable(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: UserAdd function
+func TestUserAdd(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+// TEST: Profile function
+func TestUserEdit(t *testing.T) {
+	t.Skip("Not implemented")
+}
