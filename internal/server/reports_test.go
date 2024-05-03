f41d4fcf8c20d60748dcff842fbb35b35557545b
Andres Court <andres@andrescourt.com>
2024-05-02 21:26:32 -0500

    Test the /bca/reportes/cuadre route

--- internal/server/reports_test.go
+++ internal/server/reports_test.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -107,5 +108,58 @@ func TestActualGenerate(t *testing.T) {
 		srv.ActualGenerate(response, request)
 
 		assert.Equal(t, http.StatusInternalServerError, response.Code)
+	})
+}
+
+func TestBalance(t *testing.T) {
+	t.Run("GET Method", func(t *testing.T) {
+		db := mocks.NewServiceMock()
+		_, srv := server.NewServer(db)
+
+		db.On("Levels", uuid.UUID{}).Return([]types.Select{
+			{
+				Key:   "1",
+				Value: "1",
+			},
+		})
+
+		db.On("GetActiveProjects", uuid.UUID{}, true).Return([]types.Project{
+			{
+				ID:        uuid.UUID{},
+				Name:      "1",
+				CompanyId: companyId,
+				IsActive:  &trueValue,
+			},
+		}, nil)
+
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/bca/reportes/cuadre", nil)
+
+		srv.Balance(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.Contains(t, response.Body.String(), "Cuadre")
+	})
+
+	t.Run("POST Method", func(t *testing.T) {
+		db := mocks.NewServiceMock()
+		_, srv := server.NewServer(db)
+
+		form := url.Values{}
+		form.Add("proyecto", projectId.String())
+		form.Add("date", "2022-01-01")
+		reader := strings.NewReader(form.Encode())
+
+		date := time.Date(0001, 1, 1, 0, 0, 0, 0, time.UTC)
+
+		db.On("GetBalance", uuid.UUID{}, uuid.UUID{}, date).Return(types.BalanceResponse{})
+
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodPost, "/bca/reportes/cuadre", reader)
+
+		srv.Balance(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+
 	})
 }
