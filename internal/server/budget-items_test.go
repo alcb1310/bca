2b391ae0434c93ca9652c05daa72360e75aea58e
Andres Court <andres@andrescourt.com>
2024-05-04 11:16:28 -0500

    Test the /bca/partials/budget-item/{id} route

--- internal/server/budget-items_test.go
+++ internal/server/budget-items_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 
 	"bca-go-final/internal/server"
@@ -148,4 +149,143 @@ func TestBudgetItemsAdd(t *testing.T) {
 	srv.BudgetItemAdd(response, request)
 	assert.Equal(t, http.StatusOK, response.Code)
 	assert.Contains(t, response.Body.String(), "Agregar Partida")
+}
+
+func TestBudgetItemEdit(t *testing.T) {
+	budgetItemId := uuid.New()
+	testURL := fmt.Sprintf("/bca/partials/budget-item/%s", budgetItemId.String())
+	muxVars := make(map[string]string)
+	muxVars["id"] = budgetItemId.String()
+
+	budgetItem := types.BudgetItem{
+		CompanyId:  uuid.UUID{},
+		ID:         uuid.UUID{},
+		Code:       "code",
+		Name:       "name",
+		ParentId:   nil,
+		Accumulate: sql.NullBool{Valid: true, Bool: false},
+	}
+
+	t.Run("method GET", func(t *testing.T) {
+		srv, db := server.MakeServer()
+		db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+		db.On("GetBudgetItemsByAccumulate", uuid.UUID{}, true).Return([]types.BudgetItem{})
+
+		request, response := server.MakeRequest(http.MethodGet, testURL, nil)
+		request = mux.SetURLVars(request, muxVars)
+		srv.BudgetItemEdit(response, request)
+		assert.Equal(t, http.StatusOK, response.Code)
+		assert.Contains(t, response.Body.String(), "Editar Partida")
+	})
+
+	t.Run("method PUT", func(t *testing.T) {
+		t.Run("data validation", func(t *testing.T) {
+			t.Run("code", func(t *testing.T) {
+				form := url.Values{}
+				form.Add("code", "")
+				buf := strings.NewReader(form.Encode())
+
+				srv, db := server.MakeServer()
+				db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+
+				request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+				request = mux.SetURLVars(request, muxVars)
+				srv.BudgetItemEdit(response, request)
+				assert.Equal(t, http.StatusBadRequest, response.Code)
+				assert.Contains(t, response.Body.String(), "Debe proporcionar un código de la partida")
+			})
+
+			t.Run("name", func(t *testing.T) {
+				form := url.Values{}
+				form.Add("code", budgetItem.Code)
+				form.Add("name", "")
+				buf := strings.NewReader(form.Encode())
+
+				srv, db := server.MakeServer()
+				db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+
+				request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+				request = mux.SetURLVars(request, muxVars)
+				srv.BudgetItemEdit(response, request)
+				assert.Equal(t, http.StatusBadRequest, response.Code)
+				assert.Contains(t, response.Body.String(), "Debe proporcionar un nombre de la partida")
+			})
+			t.Run("parent", func(t *testing.T) {
+				form := url.Values{}
+				form.Add("code", budgetItem.Code)
+				form.Add("name", budgetItem.Name)
+				form.Add("parent", "invalid")
+				buf := strings.NewReader(form.Encode())
+
+				srv, db := server.MakeServer()
+				db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+
+				request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+				request = mux.SetURLVars(request, muxVars)
+				srv.BudgetItemEdit(response, request)
+				assert.Equal(t, http.StatusBadRequest, response.Code)
+				assert.Contains(t, response.Body.String(), "Código de la partida padre es inválido")
+			})
+		})
+
+		t.Run("valid data", func(t *testing.T) {
+			t.Run("success", func(t *testing.T) {
+				form := url.Values{}
+				form.Add("code", budgetItem.Code)
+				form.Add("name", budgetItem.Name)
+				form.Add("parent", "")
+				buf := strings.NewReader(form.Encode())
+
+				srv, db := server.MakeServer()
+				db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+				db.On("UpdateBudgetItem", &budgetItem).Return(nil)
+				db.On("GetBudgetItems", uuid.UUID{}, "").Return([]types.BudgetItemResponse{}, nil)
+
+				request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+				request = mux.SetURLVars(request, muxVars)
+				srv.BudgetItemEdit(response, request)
+				assert.Equal(t, http.StatusOK, response.Code)
+			})
+
+			t.Run("fail", func(t *testing.T) {
+				t.Run("duplicate", func(t *testing.T) {
+					form := url.Values{}
+					form.Add("code", budgetItem.Code)
+					form.Add("name", budgetItem.Name)
+					form.Add("parent", budgetItemId.String())
+					buf := strings.NewReader(form.Encode())
+
+					budgetItem.ParentId = &budgetItemId
+					srv, db := server.MakeServer()
+					db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+					db.On("UpdateBudgetItem", &budgetItem).Return(errors.New("duplicate"))
+
+					request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+					request = mux.SetURLVars(request, muxVars)
+					srv.BudgetItemEdit(response, request)
+					assert.Equal(t, http.StatusConflict, response.Code)
+					assert.Equal(t, response.Body.String(), fmt.Sprintf("Ya existe una partida con el mismo código: %s y/o el mismo nombre: %s", budgetItem.Code, budgetItem.Name))
+					budgetItem.ParentId = nil
+				})
+
+				t.Run("unknown error", func(t *testing.T) {
+					form := url.Values{}
+					form.Add("code", budgetItem.Code)
+					form.Add("name", budgetItem.Name)
+					form.Add("parent", "")
+					buf := strings.NewReader(form.Encode())
+
+					srv, db := server.MakeServer()
+					db.On("GetOneBudgetItem", budgetItemId, uuid.UUID{}).Return(&types.BudgetItem{}, nil)
+					db.On("UpdateBudgetItem", &budgetItem).Return(UnknownError)
+
+					request, response := server.MakeRequest(http.MethodPut, testURL, buf)
+					request = mux.SetURLVars(request, muxVars)
+					srv.BudgetItemEdit(response, request)
+					assert.Equal(t, http.StatusInternalServerError, response.Code)
+					assert.Equal(t, response.Body.String(), UnknownError.Error())
+				})
+			})
+		})
+	})
 }
