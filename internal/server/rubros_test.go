e601bad1c2e0ddbb736b9f554f941af61fa9eb53
Andres Court <andres@andrescourt.com>
2024-05-05 23:28:18 -0500

    Test the /bca/partials/rubros/{id} route

--- internal/server/rubros_test.go
+++ internal/server/rubros_test.go
@@ -1,10 +1,12 @@
 package server_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 
 	"bca-go-final/internal/server"
@@ -19,4 +21,33 @@ func TestRubrosTable(t *testing.T) {
 	request, response := server.MakeRequest(http.MethodGet, testURL, nil)
 	srv.RubrosTable(response, request)
 	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestMaterialsByItem(t *testing.T) {
+	rubroId := uuid.New()
+	testURL := fmt.Sprintf("/bca/partials/rubros/%s", rubroId.String())
+	muxVars := make(map[string]string)
+	muxVars["id"] = rubroId.String()
+
+	t.Run("invalid ID", func(t *testing.T) {
+		testURL := fmt.Sprintf("/bca/partials/rubros/invalid")
+		muxVars := make(map[string]string)
+		muxVars["id"] = "invalid"
+
+		srv, _ := server.MakeServer()
+		request, response := server.MakeRequest(http.MethodGet, testURL, nil)
+		request = mux.SetURLVars(request, muxVars)
+		srv.MaterialsByItem(response, request)
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
+	t.Run("method GET", func(t *testing.T) {
+		srv, db := server.MakeServer()
+		db.On("GetMaterialsByItem", rubroId, uuid.UUID{}).Return([]types.ACU{}, nil)
+
+		request, response := server.MakeRequest(http.MethodGet, testURL, nil)
+		request = mux.SetURLVars(request, muxVars)
+		srv.MaterialsByItem(response, request)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
 }
