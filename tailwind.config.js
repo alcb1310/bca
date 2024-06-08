4e43b5a6af90bfd285b64e1a21dd96be03287bb7
Andres Court <andres@andrescourt.com>
2024-06-07 22:24:17 -0500

    Define application colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-      "./**/*.html",
-      "./**/*.go",
-      "./**/*.templ",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    module.exports = {
+        content: [
+            "./**/*.html",
+            "./**/*.go",
+            "./**/*.templ",
+        ],
+        theme: {
+            colors: {
+                'blue-gray': {
+                    50: '#eceff1',
+                    100: '#cfd8dc',
+                    200: '#b0bec5',
+                    300: '#90a4ae',
+                    400: '#78909c',
+                    500: '#607d8b',
+                    600: '#546e7a',
+                    700: '#455a64',
+                    800: '#37474f',
+                    900: '#263238',
+                },
+                'green': {
+                    500: '#22c55e',
+                    600: '#16a34a',
+                    700: '#15803d',
+                    800: '#166534',
+                    900: '#14532d',
+                }
+            },
+            extend: {
+            },
+        },
+        plugins: [],
+    }
 
