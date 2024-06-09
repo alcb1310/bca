82e9b592f268ccbb267fabe427c86bf4b87c6abe
Andres Court <andres@andrescourt.com>
2024-06-09 12:13:17 -0500

    Email & password validation

--- internals/validation/string.go
+++ internals/validation/string.go
@@ -1,5 +1,13 @@
 package validation
 
+import (
+	"fmt"
+
+	"github.com/badoux/checkmail"
+)
+
+const PASSWORD_SIZE = 3
+
 func CompanyNameValidation(name string, required bool) string {
 	if !required && name == "" {
 		return ""
@@ -11,6 +19,50 @@ func CompanyNameValidation(name string, required bool) string {
 
 	if len(name) < 3 {
 		return "El nombre debe tener al menos 3 caracteres"
+	}
+
+	return ""
+}
+
+func EmailValidator(value string, required bool) string {
+	if !required && value == "" {
+		return ""
+	}
+
+	if required && value == "" {
+		return "El email es requerido"
+	}
+
+	if err := checkmail.ValidateFormat(value); err != nil {
+		return "Ingrese un correo válido"
+	}
+
+	return ""
+}
+
+func PasswordValidator(value string, required bool) string {
+	if !required && value == "" {
+		return ""
+	}
+
+	if required && value == "" {
+		return "La contraseña es requerida"
+	}
+
+	if len(value) < PASSWORD_SIZE {
+		return fmt.Sprintf("La contraseña debe tener al menos %d caracteres", PASSWORD_SIZE)
+	}
+
+	return ""
+}
+
+func NameValidation(value string, required bool) string {
+	if !required && value == "" {
+		return ""
+	}
+
+	if required && value == "" {
+		return "es requerido"
 	}
 
 	return ""
