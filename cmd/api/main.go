d27b707de75e7bc1984f90233f153bc2c6dbe5f5
Andres Court <andres@andrescourt.com>
2024-07-26 11:10:20 -0500

    Fix conflicts

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+
 	"bca-go-final/internal/database"
 	"bca-go-final/internal/server"
 )
 
+var (
+	databaseName = os.Getenv("DB_DATABASE")
+	password     = os.Getenv("DB_PASSWORD")
+	username     = os.Getenv("DB_USERNAME")
+	dbPort       = os.Getenv("DB_PORT")
+	host         = os.Getenv("DB_HOST")
+	port         = os.Getenv("PORT")
+)
+
+func init() {
+	if databaseName == "" {
+		panic("DB_DATABASE is not set")
+	}
+
+	if password == "" {
+		panic("DB_PASSWORD is not set")
+	}
+
+	if username == "" {
+		panic("DB_USERNAME is not set")
+	}
+
+	if dbPort == "" {
+		panic("DB_PORT is not set")
+	}
+
+	if host == "" {
+		panic("DB_HOST is not set")
+	}
+
+	if port == "" {
+		panic("PORT is not set")
+	}
+
+	if os.Getenv("SECRET") == "" {
+		panic("SECRET is not set")
+	}
+}
+
 func main() {
-	db := database.New()
+	db := database.New(username, password, host, dbPort, databaseName)
 	server, _ := server.NewServer(db)
 
 	err := server.ListenAndServe()
--- internal/database/database.go
+++ internal/database/database.go
@@ -5,12 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/google/uuid"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	_ "github.com/joho/godotenv/autoload"
 
 	"bca-go-final/internal/types"
 )
@@ -123,15 +121,7 @@ type service struct {
 	db *sql.DB
 }
 
-var (
-	database = os.Getenv("DB_DATABASE")
-	password = os.Getenv("DB_PASSWORD")
-	username = os.Getenv("DB_USERNAME")
-	port     = os.Getenv("DB_PORT")
-	host     = os.Getenv("DB_HOST")
-)
-
-func New() Service {
+func New(username, password, host, port, database string) Service {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
 	db, err := sql.Open("pgx", connStr)
 	if err != nil {
