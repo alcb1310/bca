a40ee2f523cd907961cf0f6695fafb845b1d7c4d
Andres Court <andres@andrescourt.com>
2024-07-29 21:47:43 -0500

    test: public landing page test

---
+++ internal/server/routes_test.go
@@ -0,0 +1,25 @@
+package server_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"bca-go-final/internal/server"
+	"bca-go-final/mocks"
+)
+
+func TestHelloWorldHandler(t *testing.T) {
+	db := mocks.NewService(t)
+	s := server.NewServer(db)
+
+	t.Run("should open the home page", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/", nil)
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Contains(t, res.Body.String(), "Welcome")
+	})
+}
