67ec2c0b528fe344ca4dd5f2c11cf8186e9ca3b1
Andres Court <andres@andrescourt.com>
2024-08-12 18:26:13 -0500

    refactor: use the previous inserted values in the rubro's test

--- tests/integration/rubros_test.go
+++ tests/integration/rubros_test.go
@@ -11,7 +11,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -110,70 +109,44 @@ func TestRubros(t *testing.T) {
 		assert.Equal(t, http.StatusConflict, res.Code)
 		assert.Contains(t, res.Body.String(), "El rubro con código 002 y/o nombre Rubro ya existe")
 	})
-}
 
-func TestSingleRubro(t *testing.T) {
-	rubroId := uuid.MustParse("0e4b9bd3-5239-4e03-a41a-2fa0ef6363e6")
-	testUrl := fmt.Sprintf("/bca/configuracion/rubros/crear?id=%s", rubroId.String())
+	t.Run("single rubro", func(t *testing.T) {
+		companyId := getCompanyId(t, s, cookie)
+		rubros, err := s.DB.GetAllRubros(companyId)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(rubros))
+		rubroId := rubros[0].Id
+		testUrl := fmt.Sprintf("/bca/configuracion/rubros/crear?id=%s", rubroId.String())
 
-	ctx := context.Background()
-	pgContainer, err := postgres.Run(ctx,
-		"postgres:14.1-alpine",
-		postgres.WithDatabase("testrubros"),
-		postgres.WithUsername("test"),
-		postgres.WithPassword("test"),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second),
-		),
-		postgres.WithInitScripts(filepath.Join("..", "..", "internal", "database", "tables.sql")),
-		postgres.WithInitScripts(filepath.Join("scripts", "u000-company.sql")),
-		postgres.WithInitScripts(filepath.Join("scripts", "u001-rubros.sql")),
-	)
-	assert.NoError(t, err)
+		t.Run("it should get a single rubro", func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, testUrl, nil)
+			assert.NoError(t, err)
+			req.AddCookie(cookie[0])
+			res := httptest.NewRecorder()
+			s.Router.ServeHTTP(res, req)
+			assert.Equal(t, http.StatusOK, res.Code)
+			assert.Contains(t, res.Body.String(), "001")
+			assert.Contains(t, res.Body.String(), "Rubro")
+			assert.Contains(t, res.Body.String(), "Unit")
+		})
 
-	t.Cleanup(func() {
-		if err := pgContainer.Terminate(ctx); err != nil {
-			t.Fatalf("failed to terminate pgContainer: %s", err.Error())
-		}
+		t.Run("it should update a rubro", func(t *testing.T) {
+			form := url.Values{
+				"code": {"m001"},
+				"name": {"Updated Rubro"},
+				"unit": {"Updated unit"},
+			}
+
+			req, err := http.NewRequest(http.MethodPost, testUrl, strings.NewReader(form.Encode()))
+			assert.NoError(t, err)
+			req.AddCookie(cookie[0])
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			res := httptest.NewRecorder()
+			s.Router.ServeHTTP(res, req)
+			assert.Equal(t, http.StatusOK, res.Code)
+			assert.Contains(t, res.Body.String(), "m001")
+			assert.Contains(t, res.Body.String(), "Updated Rubro")
+			assert.Contains(t, res.Body.String(), "Updated unit")
+		})
 	})
-
-	s, cookie, err := createServer(t, ctx, pgContainer)
-	assert.NoError(t, err)
-	assert.NotNil(t, s)
-	assert.Equal(t, 1, len(cookie))
-	assert.Equal(t, "jwt", cookie[0].Name)
-	assert.NotEmpty(t, cookie[0].Value)
-
-  t.Run("it should get a single rubro", func(t *testing.T) {
-    req, err := http.NewRequest(http.MethodGet, testUrl, nil)
-    assert.NoError(t, err)
-    req.AddCookie(cookie[0])
-    res := httptest.NewRecorder()
-    s.Router.ServeHTTP(res, req)
-    assert.Equal(t, http.StatusOK, res.Code)
-    assert.Contains(t, res.Body.String(), "C001")
-    assert.Contains(t, res.Body.String(), "Test Item")
-    assert.Contains(t, res.Body.String(), "unit")
-  })
-
-  t.Run("it should update a rubro", func(t *testing.T) {
-    form := url.Values{
-      "code": {"m001"},
-      "name": {"Updated Rubro"},
-      "unit": {"Updated unit"},
-    }
-
-    req, err := http.NewRequest(http.MethodPost, testUrl, strings.NewReader(form.Encode()))
-    assert.NoError(t, err)
-    req.AddCookie(cookie[0])
-    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-    res := httptest.NewRecorder()
-    s.Router.ServeHTTP(res, req)
-    assert.Equal(t, http.StatusOK, res.Code)
-    assert.Contains(t, res.Body.String(), "m001")
-    assert.Contains(t, res.Body.String(), "Updated Rubro")
-    assert.Contains(t, res.Body.String(), "Updated unit")
-  })
 }
