a1678d5a8ac7ffe7195018d16503393fd62ae19c
Andres Court <andres@andrescourt.com>
2024-09-23 17:19:23 -0500

    refactor: Move the environment variable validation to its own file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,28 +24,7 @@ var (
 )
 
 func init() {
-	if databaseName == "" {
-		panic("DB_DATABASE must be set")
-	}
-	if password == "" {
-		panic("DB_PASSWORD must be set")
-	}
-	if username == "" {
-		panic("DB_USERNAME must be set")
-	}
-	if databasePort == "" {
-		panic("DB_PORT must be set")
-	}
-	if host == "" {
-		panic("DB_HOST must be set")
-	}
-	if port == "" {
-		panic("PORT must be set")
-	}
-	if secretKey == "" || len(secretKey) < 8 {
-		panic("SECRET must be set and of at least 8 characters")
-	}
-
+  environmentValidation()
   loggerSetup(env)
 }
 
