8069c8a7a6d2a19d108cfb64bc4ae2c0a9820878
Andres Court <andres@andrescourt.com>
2024-10-09 17:48:04 -0500

    feat: return the logged in user through the API

---
+++ internal/server/api_user.go
@@ -0,0 +1,16 @@
+package server
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/bca/internal/utils"
+)
+
+func (s *Server) GetCurrentUser(w http.ResponseWriter, r *http.Request) {
+	ctx, _ := utils.GetMyPaload(r)
+
+	user, _ := s.DB.GetUser(ctx.Id, ctx.CompanyId)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
+}
