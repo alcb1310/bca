2c8b2ec1c68ca44b12754697278b75c1150b279a
Andres Court <andres@andrescourt.com>
2024-10-14 17:16:37 -0500

    feat: update a supplier

--- internal/server/api_supplier.go
+++ internal/server/api_supplier.go
@@ -6,7 +6,10 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgconn"
+
 	"github.com/alcb1310/bca/internal/types"
 	"github.com/alcb1310/bca/internal/utils"
 )
@@ -100,4 +103,95 @@ func (s *Server) ApiCreateSupplier(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(supplier)
+}
+
+func (s *Server) ApiUpdateSupplier(w http.ResponseWriter, r *http.Request) {
+	if r.Body == http.NoBody || r.Body == nil {
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = "Invalid request body"
+
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	ctx, _ := utils.GetMyPaload(r)
+	id := chi.URLParam(r, "id")
+	parsedId, _ := uuid.Parse(id)
+
+	supplierToUpdate, err := s.DB.GetOneSupplier(parsedId, ctx.CompanyId)
+	if err != nil {
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = err.Error()
+		w.WriteHeader(http.StatusNotFound)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	var supplierCreate types.SupplierCreate
+	if err := json.NewDecoder(r.Body).Decode(&supplierCreate); err != nil {
+		errorResponse := make(map[string]string)
+		w.WriteHeader(http.StatusInternalServerError)
+		errorResponse["error"] = err.Error()
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	contactName := sql.NullString{
+		String: "",
+		Valid:  false,
+	}
+	contactEmail := sql.NullString{
+		String: "",
+		Valid:  false,
+	}
+	contactPhone := sql.NullString{
+		String: "",
+		Valid:  false,
+	}
+
+	if supplierCreate.ContactName != "" {
+		contactName = sql.NullString{
+			String: supplierCreate.ContactName,
+			Valid:  true,
+		}
+	}
+	if supplierCreate.ContactEmail != "" {
+		contactEmail = sql.NullString{
+			String: supplierCreate.ContactEmail,
+			Valid:  true,
+		}
+	}
+	if supplierCreate.ContactPhone != "" {
+		contactPhone = sql.NullString{
+			String: supplierCreate.ContactPhone,
+			Valid:  true,
+		}
+	}
+
+	supplierToUpdate.SupplierId = supplierCreate.SupplierId
+	supplierToUpdate.Name = supplierCreate.Name
+	supplierToUpdate.ContactName = contactName
+	supplierToUpdate.ContactEmail = contactEmail
+	supplierToUpdate.ContactPhone = contactPhone
+
+	if err := s.DB.UpdateSupplier(&supplierToUpdate); err != nil {
+		var e *pgconn.PgError
+		if errors.As(err, &e) && e.Code == "23505" {
+			errorResponse := make(map[string]string)
+			errorResponse["error"] = "Proveedor con ese ruc y/o nombre ya existe"
+			w.WriteHeader(http.StatusConflict)
+			_ = json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = err.Error()
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(supplierToUpdate)
 }
