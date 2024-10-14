c6d7b6baaf3dd22cb4e04f7dad5e472f3c98ae6d
Andres Court <andres@andrescourt.com>
2024-10-14 18:53:34 -0500

    feat: return an empty rubro object when fetching one rubro with 'crear'

--- internal/server/api_rubros.go
+++ internal/server/api_rubros.go
@@ -3,7 +3,11 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
+	"github.com/go-chi/chi/v5"
+
+	"github.com/alcb1310/bca/internal/types"
 	"github.com/alcb1310/bca/internal/utils"
 )
 
@@ -13,4 +17,23 @@ func (s *Server) ApiGetAllRubros(w http.ResponseWriter, r *http.Request) {
 	rubros, _ := s.DB.GetAllRubros(ctx.CompanyId)
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(&rubros)
+}
+
+func (s *Server) ApiGetRubro(w http.ResponseWriter, r *http.Request) {
+	ctx, _ := utils.GetMyPaload(r)
+	rubroId := chi.URLParam(r, "id")
+	if strings.ToLower(rubroId) == "crear" {
+		selectedRubro := types.Rubro{
+			Code:      "",
+			Name:      "",
+			Unit:      "",
+			CompanyId: ctx.CompanyId,
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(&selectedRubro)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotImplemented)
 }
