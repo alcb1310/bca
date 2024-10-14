c7830eaac4ce46d2e81f45d1bca163cb7c85453a
Andres Court <andres@andrescourt.com>
2024-10-14 17:55:54 -0500

    feat: get all items json response

--- internal/types/rubros.go
+++ internal/types/rubros.go
@@ -3,11 +3,11 @@ package types
 import "github.com/google/uuid"
 
 type Rubro struct {
-	Id        uuid.UUID
-	Code      string
-	Name      string
-	Unit      string
-	CompanyId uuid.UUID
+	Id        uuid.UUID `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	Unit      string    `json:"unit"`
+	CompanyId uuid.UUID `json:"company_id"`
 }
 
 type ACU struct {
