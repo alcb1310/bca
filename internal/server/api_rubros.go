9d1b9faa7fdfc7ac931b28aeac30a523cf89c60f
Andres Court <andres@andrescourt.com>
2024-10-14 19:44:05 -0500

    feat: create a rubro

--- internal/server/api_rubros.go
+++ internal/server/api_rubros.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"github.com/alcb1310/bca/internal/types"
 	"github.com/alcb1310/bca/internal/utils"
@@ -36,4 +38,48 @@ func (s *Server) ApiGetRubro(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func (s *Server) ApiCreateRubros(w http.ResponseWriter, r *http.Request) {
+	if r.Body == http.NoBody || r.Body == nil {
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = "Invalid request body"
+
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	ctx, _ := utils.GetMyPaload(r)
+	var rubro types.Rubro
+	if err := json.NewDecoder(r.Body).Decode(&rubro); err != nil {
+		errorResponse := make(map[string]string)
+		w.WriteHeader(http.StatusInternalServerError)
+		errorResponse["error"] = err.Error()
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+	rubro.CompanyId = ctx.CompanyId
+
+	rubroId, err := s.DB.CreateRubro(rubro)
+	if err != nil {
+		var e *pgconn.PgError
+		if errors.As(err, &e) && e.Code == "23505" {
+			errorResponse := make(map[string]string)
+			errorResponse["error"] = "Rubro con ese código y/ nombre ya existe"
+			w.WriteHeader(http.StatusConflict)
+			_ = json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = err.Error()
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	rubro.Id = rubroId
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(rubro)
 }
