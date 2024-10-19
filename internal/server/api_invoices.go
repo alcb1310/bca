c3fcc0def1c3d8213174338db5155d4b8f27e3a7
Andres Court <andres@andrescourt.com>
2024-10-19 12:41:31 -0500

    feat: Create an invoice

--- internal/server/api_invoices.go
+++ internal/server/api_invoices.go
@@ -2,10 +2,13 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"github.com/alcb1310/bca/internal/types"
 	"github.com/alcb1310/bca/internal/utils"
@@ -38,5 +41,38 @@ func (s *Server) ApiGetOneInvoice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusNotImplemented)
+func (s *Server) ApiCreateInvoice(w http.ResponseWriter, r *http.Request) {
+	slog.Debug("ApiCreateInvoice")
+	var data types.InvoiceCreate
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		slog.Error("ApiCreateInvoice", "err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = err.Error()
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+	ctx, _ := utils.GetMyPaload(r)
+	data.CompanyId = ctx.CompanyId
+
+	if err := s.DB.CreateInvoice(&data); err != nil {
+		var e *pgconn.PgError
+		if errors.As(err, &e) && e.Code == "23505" {
+			w.WriteHeader(http.StatusConflict)
+			errorResponse := make(map[string]string)
+			errorResponse["error"] = "Ya existe esa factura"
+			_ = json.NewEncoder(w).Encode(errorResponse)
+			return
+		}
+
+		slog.Error("ApiCreateInvoice", "err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		errorResponse := make(map[string]string)
+		errorResponse["error"] = err.Error()
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(data)
 }
